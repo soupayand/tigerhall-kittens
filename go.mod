c0e76c0533274f57680390b548d1d5048c909522
soupayand <soapayand@gmail.com>
2023-07-29 17:47:28 +0530

    Server setup and logger added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module tigerhall-kittens
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"github.com/joho/godotenv"
+	"net/http"
+	"os"
+	"tigerhall-kittens/logger"
+)
+
+func main() {
+	logger.InitLogger()
+	logger.LogInfo("Server Starting...........................................................")
+	err := godotenv.Load()
+	if err != nil {
+		logger.LogError(err)
+	}
+
+	logger.LogError(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+	logger.LogInfo("Server listening at port ", os.Getenv("PORT"))
+	logger.LogInfo("Server exited and released port ", os.Getenv("PORT"))
+}
